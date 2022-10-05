094ae1c1ea01b5bcfac6da56a3bd6661f73e6a62
6210110081 <6210110081@psu.ac.th>
2022-10-06 00:56:21 +0700

    deploy

--- src/script.js
+++ src/script.js
@@ -1,10 +1,7 @@
 let time = 0
 
 self.onload = () => {
-  time = new Date()
-  document.getElementById('tiles-board').addEventListener('click', () => {
-    let dTime = (new Date().getTime() - time.getTime()) / 1000
-    time = new Date()
-    document.getElementById('count').innerHTML = parseInt(document.getElementById('count').innerHTML) + 1
-  })
+    document.getElementById('tiles-board').addEventListener('click', () => {
+        document.getElementById('count').innerHTML = parseInt(document.getElementById('count').innerHTML) + 1
+    })
 }
